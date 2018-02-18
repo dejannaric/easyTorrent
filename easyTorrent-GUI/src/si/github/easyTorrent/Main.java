d49ef5847e08cc199165932b32c27526ff00a595
dejan <dejan@dejan-PC>
2018-02-18 21:35:29 +0100

    java version removed and replaced

--- easyTorrent-GUI/src/si/github/easyTorrent/Main.java
+++ easyTorrent-GUI/src/si/github/easyTorrent/Main.java
@@ -1,26 +1,20 @@
 package si.github.easyTorrent;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class Main extends Application{
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch();
+	}
+
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Rejc FAG");
+		primaryStage.show();
+		
 	}
 }
