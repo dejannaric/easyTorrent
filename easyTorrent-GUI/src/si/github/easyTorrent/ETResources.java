3d60812ef797a2572613e01bb7c7444e8d32e47e
dejan <dejan@dejan-PC>
2018-02-18 22:00:27 +0100

    TODO for resources, last todo: implement JIIRA or smth

--- easyTorrent-GUI/src/si/github/easyTorrent/ETResources.java
+++ easyTorrent-GUI/src/si/github/easyTorrent/ETResources.java
@@ -5,4 +5,11 @@ import com.sun.org.apache.bcel.internal.Constants;
 public interface ETResources extends Constants {
 	
 	String title(); 
+/*	
+ * TODO find a way to implement resources as interface not as inputstream:
+ * EXAMPLE:
+ * 
+ * ETResources res = GWT.create(ETResources.class);
+ * String ex = res.title();
+*/
 }
