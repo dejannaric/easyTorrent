4d5d7a617895ef34d3c4b7c66b7b0113796b55ec
dejan <dejan@dejan-PC>
2018-02-18 20:45:30 +0100

    Config, java fx GUI project initialization,

---
+++ easyTorrent-GUI/src/si/github/easyTorrent/MainWindow.java
@@ -0,0 +1,14 @@
+package si.github.easyTorrent;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow  extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Title");
+		primaryStage.show();
+	}
+
+}
